cad801267e8cf325657297341d981e3f76c0772e
Nisal Madura <nisalperera619@gmail.com>
2025-08-17 18:01:08 +0530

    Resolve the bugs

--- src/defectDetails.js
+++ src/defectDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import API_CONFIG from '../api'; // Import the simple API config
+import API_CONFIG from './api'; // Import the simple API config
 
 const DefectDetail = ({ id, onBack }) => {
   const [defect, setDefect] = useState(null);
